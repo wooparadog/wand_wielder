494b5d7d184a59798541971bc9006a2ef369c74b
WooParadog <guohaochuan@gmail.com>
2014-04-17 13:37:58 +0800

    reconfigure coveralls

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools.command.test import test as TestCommand
 class PyTest(TestCommand):
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = ['tests', '--cov', 'wand_wielder', '--cov-report', 'html']
+        self.test_args = ['tests', '--cov', 'wand_wielder', '--cov-report', 'term-missing']
         self.test_suite = True
 
     def run_tests(self):
@@ -39,6 +39,6 @@ setup(
     install_requires=[
         "wand >= 0.3.7",
         ],
-    tests_require=['pytest', 'python-coveralls', 'pytest-cov'],
+    tests_require=['pytest', 'pytest-cov'],
     cmdclass = {'test': PyTest},
 )
